a2214419356354f1f18208d1d701562171cf81ac
darina <21darina08@gmail.com>
2019-04-16 18:41:33 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,40 @@
+jQuery ('document').ready (function(){
+   
+    // var test1;
+    // var weather = 'sunny';
+    // if (weather == 'sunny') {
+    //     alert ('Picnik!');
+    // }
+    // else {
+    //     alert ('Stay home');
+    // }
+
+    // var test1;
+    // var weather = 'windy';
+    // var time = 'day';
+    // if ((weather == 'rainy' || weather == 'snowy') && time == 'day') {
+    //     alert ('Let`s go!' + weather + '\n' + 'Go!');
+    // }
+    // else if (weather == 'cloudy' && time == 'day') {
+    //     alert ('Go! But take an umbrella!');
+    // }
+    // else {
+    //     alert ('Stay home!' + '\n' + weather + '.' + '\n' + 'Picnik(');
+    // }
+
+    // var test1;
+    // var weather = 'sunny';
+    // var time = 'day';
+    // var message1 = 'Picnik?';
+    // var message2 = 'Yes!';
+    // if (weather == 'windy' && time == 'day') {
+    //     alert (message1 + '\n' + 'Let`s go!');
+    // }
+    //     else {
+    //         alert (message2);
+    //     }
+    //ZP
+    jQuery('button').on('click',function() {
+        alert('Click!');
+    });
+});
